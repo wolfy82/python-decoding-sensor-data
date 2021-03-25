051f3cff7b116c1f96c7b582abd87f3fe57cf92d
Aaron Roth <aarongroth@gmail.com>
2021-03-25 08:18:35 -0700

    Module Complete

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,12 +1,14 @@
 # Runner script for all modules
 from statistics import mean
+from datetime import datetime
 
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from particle_count_info import ParticleData
-from datetime import datetime
+from energy_info import EnergyData
+
 
 ##############################
 # Do not remove these two lines
@@ -69,3 +71,13 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 # Module 5 code here:
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Energy sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
